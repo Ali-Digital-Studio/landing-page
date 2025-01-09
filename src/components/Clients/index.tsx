c07ee502b8dd07804c863148b87a3982d101fba0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2025-01-09 11:16:23 -0400

    chore: Update Clients component with Swiper library and client images

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -1,54 +1,59 @@
 "use client";
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Navigation, Scrollbar, A11y, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, Heading } from "@chakra-ui/react";
+import Sliders from "../Sliders";
+import { ClientImages } from "../../utils/ClientImages";
+import { StargateColors } from "#/src/utils/Colors";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
 
-export default () => {
-    return (
-        <Box textAlign="center" py={10}>
-            <Heading as="h2" size="xl" mb={4}>
-                Nuestros clientes
-            </Heading>
-            <Box style={{ position: 'relative' }}>
-                <Swiper
-                    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-                    spaceBetween={50}
-                    slidesPerView={3}
-                    loop={true}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    onSlideChange={() => console.log('slide change')}
-                    style={{
-                        maxWidth: '1200px',
-                        margin: '0 auto',
-                        boxSizing: 'border-box',
-                    }}
-                >
-                    <SwiperSlide>
-                        Slide 1
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        Slide 2
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        Slide 3
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        Slide 4
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        Slide 5
-                    </SwiperSlide>
-                </Swiper>
-            </Box>
-        </Box>
-    );
-};
+export default function Clients() {
+  return (
+    <Box
+      textAlign="center"
+    >
+      <Heading
+        fontSize={{
+          base: 32,
+          md: 48,
+        }}
+        textAlign={"center"}
+        color={StargateColors.headingTextColor}
+        mb={12}
+      >
+        Nuestros clientes
+      </Heading>
+      <Box style={{ position: "relative" }}>
+        <Swiper
+          modules={[Navigation, Scrollbar, A11y, Autoplay]}
+          spaceBetween={30}
+          breakpoints={{
+            320: { slidesPerView: 2 },
+            768: { slidesPerView: 4 },
+            1024: { slidesPerView: 6 },
+            1200: { slidesPerView: 8 },
+          }}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          style={{
+            maxWidth: "1200px",
+            margin: "0 auto",
+            boxSizing: "border-box",
+          }}
+        >
+          {ClientImages.map((src, index) => (
+            <SwiperSlide key={index}>
+              <Sliders src={src} alt={`Logo ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </Box>
+  );
+}
